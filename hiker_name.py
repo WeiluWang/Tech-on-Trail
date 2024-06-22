1c7478cd5864bccb8b6a4e8d8336dfa2d404615f
WeiluWang <edrj1120@gmail.com>
2024-06-23 04:39:25 +0800

    new hiker_network (2018)

--- hiker_name.py
+++ hiker_name.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 # Load the CSV file
-df = pd.read_csv('TJ_emotion_data_2013_2023_04_30_2024.csv')
+df = pd.read_csv('TJ_emotion_data_2003_2012_04_22_2024.csv')
 
 # Extract the unique names from the "Hiker trail name" column
 unique_names = df['Hiker trail name'].dropna().unique().tolist()
@@ -11,7 +11,7 @@ unique_names = df['Hiker trail name'].dropna().unique().tolist()
 data = {"Hiker trail names": unique_names}
 
 # Write the unique names to a JSON file
-with open('hiker_trail_names.json', 'w') as json_file:
+with open('hiker_trail_names_2003-12.json', 'w') as json_file:
     json.dump(data, json_file, indent=4)
 
 print("JSON file created successfully!")
